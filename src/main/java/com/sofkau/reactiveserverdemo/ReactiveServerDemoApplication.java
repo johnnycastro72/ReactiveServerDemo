e58608fcc9aded5eafc5e450c3e6278ff54a0d60
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-06-04 22:21:32 -0500

    Initial commit

---
+++ src/main/java/com/sofkau/reactiveserverdemo/ReactiveServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.reactiveserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveServerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sofkau/reactiveserverdemo/ReactiveServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.reactiveserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
